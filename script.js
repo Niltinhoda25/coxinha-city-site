1ef095fda2205058fabf535cace3d58b27affb95
Niltinhoda25 <pizzariareis2026@gmail.com>
2026-03-03 00:47:07 -0300

    initial commiti

--- script.js
+++ script.js
@@ -1,55 +1,27 @@
 function copyIP(){
-  navigator.clipboard.writeText("play.coxinhacity.com");
-  alert("IP copiado!");
+navigator.clipboard.writeText("play.coxinhacity.com");
+alert("IP Copiado!");
 }
 
 function toggleMusic(){
-  const audio = document.getElementById("bgm");
-
-  if(audio.paused){
-    audio.volume = 0.5; // volume 50%
-    audio.play().then(() => {
-      console.log("Música tocando");
-    }).catch(err => {
-      console.log("Erro ao tocar:", err);
-    });
-  } else {
-    audio.pause();
-  }
+let music = document.getElementById("bgm");
+if(music.paused){
+music.play();
+}else{
+music.pause();
+}
 }
 
-/* PARTÍCULAS */
-const canvas = document.getElementById("particles");
-const ctx = canvas.getContext("2d");
+function searchMods(){
+let input = document.getElementById("searchMod").value.toLowerCase();
+let items = document.querySelectorAll(".modlist li");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let particles = [];
-
-for(let i=0;i<80;i++){
-particles.push({
-x:Math.random()*canvas.width,
-y:Math.random()*canvas.height,
-size:Math.random()*3,
-speed:Math.random()*1
+items.forEach(function(item){
+let text = item.textContent.toLowerCase();
+if(text.includes(input)){
+item.style.display = "block";
+}else{
+item.style.display = "none";
+}
 });
 }
-
-function animate(){
-ctx.clearRect(0,0,canvas.width,canvas.height);
-
-particles.forEach(p=>{
-p.y-=p.speed;
-if(p.y<0)p.y=canvas.height;
-
-ctx.fillStyle="#00ff00";
-ctx.beginPath();
-ctx.arc(p.x,p.y,p.size,0,Math.PI*2);
-ctx.fill();
-});
-
-requestAnimationFrame(animate);
-}
-
-animate();
